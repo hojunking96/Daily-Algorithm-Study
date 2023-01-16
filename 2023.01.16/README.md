7f8dac395677cd460c2520bf208404912b525063
chandle <phc09188@gmail.com>
2023-01-16 18:17:01 +0900

    2023.01.16 phc09188 commit

---
+++ 2023.01.16/README.md
@@ -0,0 +1,6 @@
+## 2023.01.16
+### 프로그래머스 
+(2023 KAKAO BLIND RECRUITMENT)
+[이모티콘 할인행사](https://school.programmers.co.kr/learn/courses/30/lessons/150368)
+
+문제 유형 : 완전탐색
---
+++ 2023.01.16/phc09188/Solution.java
@@ -0,0 +1,46 @@
+/**
+ *  프로그래머스 이모티콘 할인행사
+ *  70/100 실패
+ */
+class Solution {
+    public int[] solution(int[][] u, int[] e) {
+        int[] result = new int[2];
+        int[] cresult = new int[2];
+        int[] p = new int[e.length];
+
+        for(int start=40; start>=10; start-= 10){
+            for(int i=0; i<e.length; i++){
+                p[i] = start;
+            }
+            for(int en=0; en<e.length; en++){
+                for(int f=start; f>=10; f-=10){
+                    p[en] = f;
+                    cresult = makeResult(u,e,p);
+                    if(result[0]<cresult[0]){
+                        result[0] = cresult[0];
+                        result[1] = cresult[1];
+                    }else if(result[0]==cresult[0] && result[1]<cresult[1]){
+                        result[0] = cresult[0];
+                        result[1] = cresult[1];
+                    }
+                }
+                p[en] = start;
+            }
+        }
+        return result;
+    }
+    public int[] makeResult(int[][] u, int[] e, int[] p){
+        int[] result = new int[2];
+        for(int i=0; i<u.length; i++){
+            long emosum = 0;
+            for(int x=0; x<e.length; x++)
+                if(u[i][0]<=p[x])
+                    emosum += e[x]*(100-p[x])/100;
+            if(emosum>=u[i][1])
+                result[0]++;
+            else
+                result[1] += emosum;
+        }
+        return result;
+    }
+}
