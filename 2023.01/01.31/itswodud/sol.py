3897b77188ca1c5cf5df8774ce6c4b327389f1c5
itswodud <87844396+itswodud@users.noreply.github.com>
2023-01-31 18:56:54 +0900

    solve

---
+++ 2023.01/01.31/itswodud/sol.py
@@ -0,0 +1,26 @@
+def grade(correct_score: int) -> int:
+    return 6 if correct_score < 2 else 7-correct_score
+    
+    return result
+
+def solution(lottos, win_nums):
+    answer = []
+    
+    correct_score = 0
+    blackbox_score = 0
+    
+    for lotto_num in lottos:
+        if lotto_num == 0:
+            blackbox_score += 1 
+            continue
+        
+        for win_num in win_nums:
+            if lotto_num == win_num: 
+                correct_score += 1 
+                break
+                
+    answer.append(grade(correct_score+blackbox_score))
+    answer.append(grade(correct_score))
+
+    
+    return answer
