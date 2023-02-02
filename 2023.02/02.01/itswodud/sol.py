57f6af1ebf62d2f565c3b88463ba566de13ff18a
itswodud <87844396+itswodud@users.noreply.github.com>
2023-02-02 14:39:43 +0900

    simple is best
    ..

--- 2023.02/02.01/itswodud/sol.py
+++ 2023.02/02.01/itswodud/sol.py
@@ -1,45 +1,20 @@
-import datetime
-
-def add_month_to_datetime(datetime_string: str, add_month: int):
-    splited_datetime_string = datetime_string.split('.')
-    
-    year = int(splited_datetime_string[0])
-    month = int(splited_datetime_string[1])
-    day = int(splited_datetime_string[2])
-    
-    add_year = add_month//12
-    add_month = add_month%12
-    
-    if month+add_month == 12:
-        new_year = year
-    else:
-        new_year = year + (month+add_month)//12 if add_month != 0 else year+1 
-    new_month = 12 if (month+add_month)%12 == 0 else (month+add_month)%12
-    new_day = day
-
-    result_date = datetime.datetime(new_year, new_month, new_day) - datetime.timedelta(days=1)
-
-    if result_date.day > 28:
-        result_date = result_date.replace(day=28)
-    
-    return result_date
-    
 def solution(today, terms, privacies):
     answer = []
-    
-    term_dict = {}
-    
-    today_datetime = datetime.datetime.strptime(today, '%Y.%m.%d')
-    
-    for term in terms:
-        splited_term = term.split(' ')
-        term_dict[splited_term[0]] = int(splited_term[1])
-    
-    for i, privacy in enumerate(privacies):
-        splited_privacy = privacy.split(' ')
-        if today_datetime > add_month_to_datetime(splited_privacy[0], term_dict[splited_privacy[1]]):
-            
-            answer.append(i+1) 
-        
-    
+
+    today_y, today_m, today_d = today.split('.')
+    today = int(today_y)*12*28 + int(today_m)*28 + int(today_d)
+
+    terms_dic = {term.split(' ')[0] : int(term.split(' ')[1])*28 for term in terms}
+
+
+    for idx, p in enumerate(privacies):
+        date, term = p.split(' ')
+        y, m, d = date.split('.')
+
+        check_day = int(y)*12*28 + int(m)*28 + int(d)
+
+        if check_day+terms_dic[term] <= today:
+            answer.append(idx+1)
+
+
     return answer
